0f95f3f9a566f1b648494021e80fe03112a31369
enji-coder <47570231+enji-coder@users.noreply.github.com>
2025-07-22 23:17:54 +0530

    updates

---
+++ MAIN_DAY_1/pro1.py
@@ -0,0 +1,11 @@
+# def linear_search(arr,target):
+#     for i in range(len(arr)):
+#         if arr[i] == target:
+#             return i 
+#     return "not found"
+
+
+# arr = [5,4,3,1,8,6]
+# print(linear_search(arr,1))
+
+
