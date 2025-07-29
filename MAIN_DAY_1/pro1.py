ead906c7e97fbfa86c9632dcff0dfb77e617d15d
Anjali Patel <47570231+enji-coder@users.noreply.github.com>
2025-07-29 16:58:19 +0530

    updates

--- MAIN_DAY_1/pro1.py
+++ MAIN_DAY_1/pro1.py
@@ -1,11 +1,34 @@
-# def linear_search(arr,target):
-#     for i in range(len(arr)):
-#         if arr[i] == target:
-#             return i 
-#     return "not found"
+# Define one coach = node
+class Node:
+    def __init__(self, data):
+        self.data = data      # Passenger in Coach
+        self.next = None      # Link to next Coach
 
+# Train = Linked List
+class LinkedList:
+    def __init__(self):
+        self.head = None  # First Coach (head of train)
 
-# arr = [5,4,3,1,8,6]
-# print(linear_search(arr,1))
+    def add(self, data):
+        new_node = Node(data)
+        if not self.head:  # If train is empty
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:   # Go to the last Coach
+            temp = temp.next
+        temp.next = new_node  # Link new Coach at the end
 
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
 
+# Test the train
+train = LinkedList()
+train.add("Coach A")
+train.add("Coach B")
+train.add("Coach C")
+train.display()
